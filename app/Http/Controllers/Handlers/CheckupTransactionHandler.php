5c0a8408204944ad3e952b9767a87c95b3bb3476
fuadmmnf <bsse0920@iit.du.ac.bd>
2020-07-26 04:49:00 +0600

    added first appointment rate for patient appointment

--- app/Http/Controllers/Handlers/CheckupTransactionHandler.php
+++ app/Http/Controllers/Handlers/CheckupTransactionHandler.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Handlers;
 
 
 use App\Doctor;
+use App\Doctorappointment;
 use App\Patient;
 use App\Patientcheckup;
 use App\Transaction;
@@ -16,18 +17,17 @@ class CheckupTransactionHandler
 {
     private $userCreditLimit = 100;
 
-    private function createUserDebitTransaction(User $user, $amount, $status): Transaction{
+    private function createUserDebitTransaction(User $user, $amount, $status): Transaction
+    {
         $checkupTransaction = new Transaction();
         $checkupTransaction->user_id = $user->id;
         $checkupTransaction->type = 0; // 0 => debit, 1 => credit
         $checkupTransaction->status = $status; // 0 => initialized, 1 => complete
         $checkupTransaction->amount = $amount;
-        do
-        {
+        do {
             $code = Str::random(16);
             $transaction = Transaction::where('code', $code)->first();
-        }
-        while($transaction);
+        } while ($transaction);
         $checkupTransaction->code = $code;
 
         $checkupTransaction->save();
@@ -38,30 +38,40 @@ class CheckupTransactionHandler
     }
 
 
-    public function checkUserAccountBalanceAndProceedTransaction(User $user, Doctor $doctor, $status): ?Transaction
+    public function checkUserAccountBalanceAndProceedTransaction(Patient $patient, Doctor $doctor, $status): ?Transaction
     {
-        $remainingAmountAfterTransaction = $user->balance + $this->userCreditLimit - $doctor->offer_rate;
+        $user = $patient->user;
+        $rate = $doctor->offer_rate;
+        if ($status == 0) {
+            $checkupIds = Patientcheckup::where('patient_id', $patient->id)
+                ->where('doctor_id', $doctor->id)
+                ->pluck('id');
+            $patientPreviousAppointment = Doctorappointment::whereIn('patientcheckup_id', $checkupIds)->first();
+            if (!$patientPreviousAppointment && $doctor->first_appointment_rate != null) {
+                $rate = $doctor->first_appointment_rate;
+            }
 
-        if($remainingAmountAfterTransaction >= 0){
-            return $this->createUserDebitTransaction($user, $doctor->offer_rate, $status);
-        } else{
+        }
+
+        $remainingAmountAfterTransaction = $user->balance + $this->userCreditLimit - $rate;
+
+        if ($remainingAmountAfterTransaction >= 0) {
+            return $this->createUserDebitTransaction($user, $rate, $status);
+        } else {
             return null;
         }
     }
 
 
-
-    public function createNewCheckup(Patient $patient, Doctor $doctor, ?Carbon $start_time,  ?Carbon $end_time): ?Patientcheckup
+    public function createNewCheckup(Patient $patient, Doctor $doctor, ?Carbon $start_time, ?Carbon $end_time): ?Patientcheckup
     {
-        $user = $patient->user;
-
         $newPatientcheckup = new Patientcheckup();
         $newPatientcheckup->patient_id = $patient->id;
         $newPatientcheckup->doctor_id = $doctor->id;
         $newPatientcheckup->start_time = $start_time;
         $newPatientcheckup->end_time = $end_time;
 
-        $transaction = $this->checkUserAccountBalanceAndProceedTransaction($user, $doctor, $start_time != null);
+        $transaction = $this->checkUserAccountBalanceAndProceedTransaction($patient, $doctor, $start_time != null);
 
         if ($transaction) {
             $newPatientcheckup->transaction_id = $transaction->id;
