0fcaa6ea4e6730b2046e70198a53c986dd297126
fuadmmnf <bsse0920@iit.du.ac.bd>
2020-08-23 20:46:03 +0600

    fixed auth admin

--- app/gcf/functions/index.js
+++ app/gcf/functions/index.js
@@ -7,3 +7,6 @@ const functions = require('firebase-functions');
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+exports.myFunction = functions.firestore
+    .document('doctorcall/{doc_id}')
+    .onWrite((change, context) => { /* ... */ });
