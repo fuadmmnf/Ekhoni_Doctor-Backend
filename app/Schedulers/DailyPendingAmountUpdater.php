00dff9ed0480a1f21836f9e4eb232f21da943de6
fuadmmnf <bsse0920@iit.du.ac.bd>
2020-10-03 14:49:50 +0600

    fixed scheduler to get percentage for day before

--- app/Schedulers/DailyPendingAmountUpdater.php
+++ app/Schedulers/DailyPendingAmountUpdater.php
@@ -8,6 +8,7 @@ use App\Agentpayments;
 use App\Doctor;
 use App\Doctorpayments;
 use App\Patientcheckup;
+use Carbon\Carbon;
 
 class DailyPendingAmountUpdater
 {
@@ -22,6 +23,7 @@ class DailyPendingAmountUpdater
         $doctors = Doctor::where('activation_status', 1)->get();
         foreach ($doctors as $doctor) {
             $doctorcheckups = Patientcheckup::where('doctor_id', $doctor->id)
+                ->whereDate('start_time', Carbon::now()->subDay())
                 ->where('status', 1)
                 ->get();
             $doctorCheckupAmount = 0;
@@ -29,8 +31,8 @@ class DailyPendingAmountUpdater
                 $doctorCheckupAmount += $doctorcheckup->transaction->amount;
             }
             $doctorCheckupAmount = $doctorCheckupAmount - ($doctorCheckupAmount * $doctor->commission);
-            $doctorPaymentsAmount = Doctorpayments::where('doctor_id', $doctor->id)->get()->sum('amount');
-            $doctor->pending_amount = $doctorCheckupAmount - $doctorPaymentsAmount;
+//            $doctorPaymentsAmount = Doctorpayments::where('doctor_id', $doctor->id)->get()->sum('amount');
+            $doctor->pending_amount = $doctor->pending_amount + $doctorCheckupAmount;
             $doctor->save();
         }
     }
@@ -41,15 +43,16 @@ class DailyPendingAmountUpdater
         foreach ($agents as $agent) {
             $agentPatients = $agent->patients()->pluck('id');
             $agentCheckups = Patientcheckup::where('patient_id', $agentPatients)
+                ->whereDate('start_time', Carbon::now()->subDay())
                 ->where('status', 1)
                 ->get();
             $agentCheckupAmount = 0;
             foreach ($agentCheckups as $agentcheckup) {
                 $agentCheckupAmount += $agentcheckup->transaction->amount;
             }
-            $agentCheckupAmount = $agentCheckupAmount - ($agentCheckupAmount * $agent->agent_percentage);
-            $agentPaymentsAmount = Agentpayments::where('agent_id', $agent->id)->get()->sum('amount');
-            $agent->pending_amount = $agentCheckupAmount - $agentPaymentsAmount;
+            $agentCheckupAmount = $agentCheckupAmount * $agent->agent_percentage;
+//            $agentPaymentsAmount = Agentpayments::where('agent_id', $agent->id)->get()->sum('amount');
+            $agent->pending_amount = $agent->pending_amount + $agentCheckupAmount;
             $agent->save();
         }
     }
