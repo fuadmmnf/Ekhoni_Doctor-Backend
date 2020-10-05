cf18ebc0cebf405a2715ad0d5a6f9be98bf9c7f4
fuadmmnf <bsse0920@iit.du.ac.bd>
2020-10-05 12:35:00 +0600

    changed agent convert validation

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -320,7 +320,7 @@ class UserController extends Controller
         }
         $validator = Validator::make($request->all(), [
             'is_agent' => 'required',
-            'agent_percentage' => 'required',
+            'agent_percentage' => 'required| numeric| min: 0| max: 100',
         ]);
         if ($validator->fails()) {
             return response()->json('validation error', 400);
